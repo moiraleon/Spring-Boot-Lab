1310cc9ecc355721013ff77fe63baa985806d11b
Moira Leon <moira.leon2@gmail.com>
2021-08-30 17:10:22 -0600

    Adding Unit Testing

--- src/test/java/com/example/springinitializrlab/SpringInitializrLabApplicationTests.java
+++ src/test/java/com/example/springinitializrlab/SpringInitializrLabApplicationTests.java
@@ -2,12 +2,50 @@ package com.example.springinitializrlab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-class SpringInitializrLabApplicationTests {
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class SpringInitializrLabApplicationTests {
+//Adding Unit Test Part 1
+	//	@Test
+//	void contextLoads() {
+//	}
+//	@Autowired
+//	private MockMvc mvc;
+//
+//	@Test
+//	public void getHello() throws Exception {
+//		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isOk())
+//				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+//	}
+//Adding Unit Test Part 2
+	@Autowired
+	private TestRestTemplate template;
 
 	@Test
-	void contextLoads() {
-	}
+	public void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity("/", String.class);
+		assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
 
+	}
 }
+
+
