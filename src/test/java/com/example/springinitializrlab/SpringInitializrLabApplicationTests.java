60aee2975730f2137b455d21bc1db1a01fd0cb75
Moira Leon <moira.leon2@gmail.com>
2021-08-30 16:07:18 -0600

    Create a Simple Web Application

---
+++ src/test/java/com/example/springinitializrlab/SpringInitializrLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinitializrlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
